a3e0d107a0968cfc22a528ef672bae4fd9c3b4ec
Creator <doctor.chuma.1999@gmail.com>
2024-03-05 03:04:08 +0300

    v1

--- blog/models.py
+++ blog/models.py
@@ -14,20 +14,23 @@ class Blog_News(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     blog = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
-    preview = models.ImageField(upload_to='blog_previews/')
+    preview = models.ImageField(upload_to='blog_previews/',blank=True)
+    
+    class Meta:
+        ordering = ['-publish']    
+    
     
 @receiver(pre_save, sender=Blog_News)
 def generate_slug(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = slugify(instance.title)
         
-    class Meta:
-        ordering = ['-publish']
+
 
      
 
 class Subsection(models.Model):
     blog_news = models.ForeignKey(Blog_News, on_delete=models.CASCADE, related_name='subsections')
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100,blank=True)
     content = models.TextField()
     image = models.ImageField(upload_to='subsection_images/', null=True, blank=True)
